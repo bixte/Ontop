6f0a4361dfb99401bf0f8f08735e02110f0d8e8e
bixTry <cccrankboy@gmail.com>
2021-09-27 06:39:31 +0300

    main

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,33 @@
-console.log("hello");
+document.addEventListener('DOMContentLoaded', ()=>{
+    let graphNav = document.querySelector ('.analytic-date__navigation');
+    let grathBtns = document.querySelectorAll('[data-graph]')
+    console.log(graphNav);
+    graphNav.addEventListener ('click', function showGraph(){
+        grathBtns.forEach(grathBtn => {
+        });
+    })
+
+    let progressChart = {
+        type: "bar",
+        series: [{
+            
+        values: [87,70,116,20],
+        'border-radius': "9px",
+        styles: [ "#F0AD00", "#F09000", "#5BBF38", "#F06500"] 
+        }   
+           
+        ],
+          scaleX: {
+            values: ['Неделя 1', 'Неделя 2', 'Неделя 3', 'Неделя 4'],
+        }
+   }
+   
+   zingchart.render({
+       id : 'myChart',
+       data : progressChart,
+       height: 400,
+       width: '100%',
+       modules: "toolbar-zoom"
+   });
+
+})
